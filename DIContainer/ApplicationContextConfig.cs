35e3f6ef4a11201ef8405cbd345cd13038a5d49f
Yuri Voitovich <voitovich200233@gmail.com>
2022-10-05 04:23:33 +0300

    Добавьте файлы проекта.

---
+++ DIContainer/ApplicationContextConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DIContainer
+{
+    public class ApplicationContextConfig
+    {
+        private IDictionary<Type, IDictionary<ImplementationEnum, Type>> container = new Dictionary<Type, IDictionary<ImplementationEnum, Type>>();
+
+        public void Register(ImplementationEnum implNumber)
+        {
+
+        }
+    }
+}
---
+++ DIContainer/ImplementationEnum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIContainer
+{
+    public enum ImplementationEnum
+    {
+        First,
+        Second,
+        Third,
+        Fourth,
+        Five,
+        Sixth,
+        Seven,
+        Eight,
+        Nine
+    }
+}
